4d7e3d0ceb0c648df3d431d8e970e5b30b04f193
naquinem <marlonagba@gmail.com>
2024-02-18 14:07:09 +0800

    Add fake data in backend

---
+++ backend/database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+
+            'item_code' => fake()->bothify('??#????##'),
+            'description' => fake()->text(),
+            'quantity' => fake()->randomDigit(),
+            'price' => fake()->randomDigit(),
+
+        ];
+    }
+}
